1745ad9c33cd96dd5add8e44ee6322aa259fa812
vic <murimimlvictor@gmail.com>
2023-09-14 16:39:07 +0300

    connect from config file

--- src/main.c
+++ src/main.c
@@ -3,39 +3,21 @@
 #include "./include/list.h"
 #include "./include/map.h"
 #include "./include/lib.h"
-//#include "./json.h"
 #include "./include/requests.h"
 #include "./include/dbc.h"
 #include <json-c/json.h>
 
 int main()
 {
-    struct json_object * obj1,*obj2,*res;
-    obj1 = json_object_new_object();
-    obj2 = json_object_new_object();
+  MYSQL * sql = NULL;
+  sql = mysql_init(sql);
 
-    json_object_object_add(obj1,"one",json_object_new_int(123));
-    json_object_object_add(obj2,"two",json_object_new_int(432));
+  if((create_connection_from_a_file(&sql,"/home/vic/Desktop/ev2/events/config/config.json")) == NULL)
+  {
+    puts("connection failed");
+    mysql_close(sql);
+    exit(EXIT_FAILURE);
+  }
 
-    printf("%s\n",json_object_to_json_string(obj1));
-    printf("%s\n",json_object_to_json_string(obj2));
-
-    struct json_object * sub;
-
-    json_object_object_get_ex(obj1,"one",&sub);
-    
-
-    res = json_object_new_array();
-
-    json_object_array_add(res,obj1);
-    json_object_array_add(res,obj2);
-
-    printf("%s\n",json_object_to_json_string(res));
-
-    json_object_put(obj1);
-    json_object_put(obj2);
-    json_object_put(res);
-
-    
-    
+  mysql_close(sql);
 }
