6198d52105f68307a9fbb086a78edd5220fd3dfd
vic <murimimlvictor@gmail.com>
2023-09-26 12:51:33 +0300

    remove builds

--- build/src/source/data-structures/list.c.d
+++
@@ -1,8 +0,0 @@
-build/./src/source/data-structures/list.c.o: \
- src/source/data-structures/list.c \
- src/source/data-structures/../../include/data-structures/list.h \
- src/source/data-structures/../../include/data-structures/../lib/ev_strings.h \
- src/source/data-structures/../../include/data-structures/../lib/../data-structures/ev_string.h
-src/source/data-structures/../../include/data-structures/list.h:
-src/source/data-structures/../../include/data-structures/../lib/ev_strings.h:
-src/source/data-structures/../../include/data-structures/../lib/../data-structures/ev_string.h:
--- build/src/source/threads/pool.c.d
+++
@@ -1,3 +0,0 @@
-build/./src/source/threads/pool.c.o: src/source/threads/pool.c \
- src/source/threads/../../include/threads/pool.h
-src/source/threads/../../include/threads/pool.h:
