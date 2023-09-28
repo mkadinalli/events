6bfafe8f2c56b4b0a384eb5b7ad3497c0c602dcf
vic <murimimlvictor@gmail.com>
2023-09-28 10:40:04 +0300

    add dinamic for user

--- src/source/da/users-da.c
+++ src/source/da/users-da.c
@@ -5,6 +5,23 @@
 bool insert_into_users(const char *name, const char *username, const char *email, const char *password)
 {
     char *query = "insert into users (name,username,email,pass_word) values (?,?,?,?)";
+
+    result_bind * rb = result_bind_create(6);
+    result_bind_set_string(0,rb,name);
+    result_bind_set_string(1,rb,username);
+    result_bind_set_string(2,rb,email);
+    result_bind_set_string(3,rb,password);
+    json_object *res =  execute_prepared_query(query,rb);
+
+    result_bind_destroy(rb);
+
+    if(res)
+    {
+        json_object_put(res);
+        return true;
+    }
+
+    return false;
 }
 
 
@@ -18,6 +35,26 @@ bool update_one_user(const char *name,
 {
     char *query = "update users set name = ?,username = ?,email = ? ,avater = ?, bio = ? ,about = ? where id = uuid_to_bin(?)";
 
+    result_bind * rb = result_bind_create(7);
+    result_bind_set_string(0,rb,name);
+    result_bind_set_string(1,rb,username);
+    result_bind_set_string(2,rb,email);
+    result_bind_set_string(3,rb,avater);
+    result_bind_set_string(4,rb,bio);
+    result_bind_set_string(5,rb,about);
+    result_bind_set_string(6,rb,id);
+
+    json_object *res =  execute_prepared_query(query,rb);
+
+    result_bind_destroy(rb);
+    
+    if(res)
+    {
+        json_object_put(res);
+        return true;
+    }
+
+    return false;
 }
 
 
@@ -25,6 +62,15 @@ bool update_one_user(const char *name,
 json_object *get_user(char *id_)
 {
     char *query = "call get_user(?)";
+
+    result_bind * rb = result_bind_create(1);
+    result_bind_set_string(0,rb,id_);
+
+    json_object *res =  execute_prepared_call_query(query,rb);
+
+    result_bind_destroy(rb);
+
+    return res;
 }
 
 
