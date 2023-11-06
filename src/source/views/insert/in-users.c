6e1c0f5784028724eddd1216d8605119facb6ea4
vic <murimimlvictor@gmail.com>
2023-11-06 20:19:34 +0300

    edit v url

--- src/source/views/insert/in-users.c
+++ src/source/views/insert/in-users.c
@@ -71,7 +71,7 @@ void sign_up(SSL *sock, char *json_load)
 
     char v_url[200];
 
-    sprintf(v_url, "Click on this link to verify <a href=\"https://localhost:2000/api/verify/?token=%s&id=%s\">Verify</a>",
+    sprintf(v_url, "Click on this link to verify <a href=\"http://localhost:3000/api/verify/%s/%s\">Verify</a>",
             json_object_get_string(token),
             json_object_get_string(id));
 
